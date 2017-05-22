6034be92672b38099b7f5dd0dc313ee3f990979b
Lnannan <1154837641@qq.com>
2017-05-22 17:42:22 +0800

    sasas

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+//接值
+var gulp=require('gulp');
+var gulputil=require('gulp-util');
+var uglify=require('gulp-uglify');
+var cssmin=require('gulp-clean-css');
+var minifyHtml=require('gulp-minify-html');
+
+
+gulp.task('hello', function () {
+console.log('hello');
+});
+
+gulp.task('uglifyjs', function(){
+gulp.src('js/*.js').pipe(uglify()).pipe(gulp.dest('xiangmu/js'))
+});
+
+
+gulp.task('myminifycss', function () {
+gulp.src('css/*.css')
+.pipe(cssmin())
+.pipe(gulp.dest('xiangmu/css'));
+});
+
+gulp.task('myminifyhtml', function () {
+gulp.src('*.html') // 要压缩的html文件
+.pipe(minifyHtml()) //压缩
+.pipe(gulp.dest('xiangmu'));
+});
+
+gulp.task('default',['hello','uglifyjs','myminifycss','myminifyhtml']);
